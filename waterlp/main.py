366a8c1cc6883a92f1726367d8c29408d60ecab3
David Rheinheimer <rheinheimer@users.noreply.github.com>
2019-06-07 14:47:35 -0400

    update user path

--- waterlp/main.py
+++ waterlp/main.py
@@ -7,16 +7,18 @@ import getpass
 
 from waterlp.tasks import run_model
 from waterlp.parser import commandline_parser
+from pathlib import Path
+home = str(Path.home())
 
 if __name__ == '__main__':
     try:
         parser = commandline_parser()
         args, unknown = parser.parse_known_args(sys.argv[1:])
 
-        app_dir = '/home/{}/.waterlp'.format(getpass.getuser())
+        app_dir = os.path.join(home, '.waterlp')
         if os.path.exists(app_dir):
             shutil.rmtree(app_dir)
-        logs_dir = '{}/logs'.format(app_dir)
+        logs_dir = os.path.join(app_dir, 'logs')
 
         run_model(args, logs_dir)
     except Exception as e:
