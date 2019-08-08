47ba70667fbc590b6f5a767f5eae7882583560ad
Dan <dantran2016@gmail.com>
2019-08-07 17:22:23 -0700

    Added script that runs models with climate change scenarios

--- examples/Merced_Model/generate_metrics.py
+++ examples/Merced_Model/generate_metrics.py
@@ -4,12 +4,18 @@ from hydroeval import *
 
 # Change this to check parameter ex: ""
 model_parameter = "node/Near Stevinson_11272500/flow"
-observed_parameter = "node/Near Stevinson_11272500/observed flow"
 
+# Generate the observed parameter name
+split_value = model_parameter.split("/")
+split_value[-1] = "observed " + split_value[-1]
+observed_parameter = "/".join(split_value)
+
+# Getting parameter values and changing them to numpy
 results_csv = pd.read_csv("merced/results.csv", skiprows=[1])
 model_data = results_csv[model_parameter].values
 observed_data = results_csv[observed_parameter].values
 
+# Calculate the metrics
 nse_value = float(evaluator(nse, model_data, observed_data))
 print("The NSE value of {} is : {}".format(model_parameter, nse_value))
 p_bias = float(evaluator(pbias, model_data, observed_data))
---
+++ examples/run_climate_change.py
@@ -0,0 +1,44 @@
+import os
+import subprocess
+import shutil
+import pandas as pd
+import numpy as np
+
+# Transfer the  TOT_RUNOFF files to the correct directory
+# Run the model in using a subprocess
+# Append the results from that CSV to a new CSV with a new file name
+
+model_name = "merced"
+model_folder = "Merced_Model"
+model_run_script = "run_merced_model.py"
+climate_change_folder = "Climate Change"
+climate_change_dest = "s3_imports"
+
+bucket = 'openagua-networks'
+dest = os.getcwd() + "\\" + model_folder + "\\" + model_name + "\\" + climate_change_dest
+root_dir = os.path.join(os.getcwd(), model_name)
+model_path = os.path.join(root_dir, 'pywr_model.json')
+network_key = os.environ.get('NETWORK_KEY')
+
+def add_csv():
+    pass
+def run_model():
+    os.chdir(os.getcwd() + "\\" + model_folder)
+    proc = subprocess.run(['python', os.getcwd() + "\\" + model_run_script,
+                           model_path, root_dir, bucket, network_key], stdout=subprocess.PIPE)
+
+
+# Get all folders in climate change
+folder_names = next(os.walk(os.getcwd() + "\\" + climate_change_folder))[1]
+
+for folder in folder_names:
+    # Copy the Runoff files to the model
+    src_files = os.listdir(os.getcwd() + "\\" + climate_change_folder + "\\" + folder)
+    for file_name in src_files:
+        file_path = os.path.join(os.getcwd() + "\\" + climate_change_folder + "\\" + folder, file_name)
+        shutil.copy(file_path, dest)
+
+    # Run the model
+    run_model()
+    os.chdir("..")
+
