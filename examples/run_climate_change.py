d254c3fc2ce2f0297b046f5235f307c8c69f4545
Dan <dantran2016@gmail.com>
2019-08-14 09:42:30 -0700

    Finished climate scenarios script

--- examples/run_climate_change.py
+++ examples/run_climate_change.py
@@ -9,7 +9,7 @@ import numpy as np
 # Append the results from that CSV to a new CSV with a new file name
 
 model_name = "merced"
-model_folder = "Merced_Climate`"
+model_folder = "Merced_Climate"
 model_run_script = "run_merced_model.py"
 climate_change_folder = "Climate Change"
 climate_change_dest = "s3_imports"
@@ -19,9 +19,17 @@ dest = os.getcwd() + "\\" + model_folder + "\\" + model_name + "\\" + climate_ch
 root_dir = os.path.join(os.getcwd(), model_name)
 model_path = os.path.join(root_dir, 'pywr_model.json')
 network_key = os.environ.get('NETWORK_KEY')
+final_csv = None
 
-def add_csv():
-    pass
+def add_data(final_csv, prefix):
+    results_csv = pd.read_csv(os.getcwd() + "\\" + model_folder + "\\" + model_name + "\\" + "results.csv")
+    results_csv.add_prefix(prefix)
+    if final_csv is None:
+        final_csv = results_csv
+    else:
+        final_csv.join(results_csv)
+    return final_csv
+
 def run_model():
     os.chdir(os.getcwd() + "\\" + model_folder)
     proc = subprocess.run(['python', os.getcwd() + "\\" + model_run_script,
@@ -42,3 +50,7 @@ for folder in folder_names:
     run_model()
     os.chdir("..")
 
+    prefix = folder
+    final_csv = add_data(final_csv, prefix)
+
+final_csv.to_csv(root_dir + "\\climate_change.csv")
