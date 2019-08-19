f77bf3dd9cd62fe61300490948e29d2f00ea89a4
Dan <dantran2016@gmail.com>
2019-08-19 14:46:03 -0700

    Added Merced Model figures

--- examples/Merced_Model/calibrate_model.py
+++ examples/Merced_Model/calibrate_model.py
@@ -1,16 +1,16 @@
 import spotpy
-from Merced_Model.spot_setup_merced import SpotSetup
+from spot_setup_merced import SpotSetup
 
 # Set local storage variables for calibration ie: Name and Format(CSV, RAM)
 dbname = "lake_mcclure_storage"
 dbformat = "csv"
 
-rep = 4
+rep = 50
 results = []
 spot_setup = SpotSetup()
 
 # Setup the environment with a markov chain monte carlo algorithm
-sampler = spotpy.algorithms.mcmc(spot_setup, dbformat=dbformat, dbname=dbname)
+sampler = spotpy.algorithms.lhs(spot_setup, dbformat=dbformat, dbname=dbname)
 
 # Calibrate the model over "rep" iterations
 sampler.sample(rep)
