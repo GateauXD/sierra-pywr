ab6cb07a23e3a9c8151eba6ee0457b2cde075331
Dan <dantran2016@gmail.com>
2019-08-23 11:09:21 -0700

    Added box graphs and box graph scripts.

---
+++ examples/Merced_Climate/graph_box.py
@@ -0,0 +1,34 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import matplotlib.dates as mdates
+import seaborn as sns
+
+date_of_interest = [1980,2060]
+item_of_interest = "Lake McClure Inflow/flow"
+
+def generate_csv():
+    results_csv = pd.read_csv("MERR_inflow_LakeMcClure_RCP45.csv", index_col=[4])
+    results_csv = results_csv.loc["{}-10".format(date_of_interest[0]):"{}-9".format(date_of_interest[1])]
+    graph_melt = pd.melt(results_csv,
+                         id_vars=["Year", "Scen", "Month"],
+                         value_vars=["Inflow"],
+                         value_name="values"
+                         )
+    return graph_melt
+
+
+graph_data = generate_csv()
+plt.rcParams["figure.figsize"] = [16,9]
+
+time = ["Near Future (2030-2060)", "Far Future (2070-2100)"]
+index = 0
+
+# Inflow from Lake McClure
+# RCP 4.8
+sns.set(style="whitegrid", rc={'figure.figsize': (20.7, 13.27)}, font_scale=1.5)
+sns.boxplot(x="Month", y="values", hue="Scen", data=graph_data)
+plt.title("Lake McClure Monthly Inflow - All Scenarios RCP 4.5 - {}".format(time[index]))
+plt.xlabel("Time")
+plt.ylabel("Flow Value")
+plt.savefig("Figures/{}_monthly_inflow_all_4.5.png".format(time[index].split()[0].lower()))
+plt.show()
--- examples/Merced_Climate/graph_results.py
+++ examples/Merced_Climate/graph_results.py
@@ -1,32 +1,16 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-from pylab import rcParams
+import matplotlib.dates as mdates
+import seaborn as sns
 
-col_names = []
-item_of_interest = "Lake McClure/storage"
+date_of_interest = [2030, 2060]
+item_of_interest = "Lake McClure Inflow/flow"
 
-def generate_csv():
-    results_csv = pd.read_csv("climate_change.csv", skiprows=[1])
-    return_csv = results_csv.filter(regex=item_of_interest)
-    global col_names
-    col_names = [col for col in results_csv.columns if item_of_interest in col]
+historic_csv = pd.read_csv("C:\\Users\\GateauXD\\Documents\\GitHub\\waterlp-pywr2-dan\\examples\\Merced_Model\\Graph.csv")
+climate_change_csv = pd.read_csv("Graph.csv")
 
-    # Simplify the Column names
-    for col_name in col_names:
-        new_name = col_name.split("/")[0]
-        return_csv.rename(columns={col_name: new_name}, inplace=True)
+plt.rcParams["figure.figsize"] = [16,9]
 
-    return return_csv
+time = ["Near Future (2030-2060)", "Far Future (2070-2100)"]
+index = 0
 
-graph_data = generate_csv()
-
-rcParams['figure.figsize'] = 24, 12
-fig = plt.figure()
-ax = plt.gca()
-plt.title("Climate Change Scenarios for Lake McClure Storage Value")
-plt.ylabel("Storage Value (mcm)")
-# for col_name in graph_data.columns:
-#     graph_data.plot(kind="line", y=col_name, ax=ax)
-
-graph_data.boxplot()
-plt.show()
