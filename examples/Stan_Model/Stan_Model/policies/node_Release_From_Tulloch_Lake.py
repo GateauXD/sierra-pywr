e57cc00b83156e0d75ddbd83cded4a1cc5e1ecec
Dan <dantran2016@gmail.com>
2019-09-17 14:53:21 -0700

    Added Release logic for Tulloch
    - Need to get NetDemand reading from CSV file working

--- examples/Stan_Model/Stan_Model/policies/node_Release_From_Tulloch_Lake.py
+++ examples/Stan_Model/Stan_Model/policies/node_Release_From_Tulloch_Lake.py
@@ -1,18 +1,28 @@
 import math
 import pandas as pd
 import numpy as np
+from utilities import get_year_type
 from parameters import WaterLPParameter
 from datetime import datetime, timedelta
 
 
 class node_Release_From_Tulloch_Lake(WaterLPParameter):
 
+    def get_demand(self, timestep, scenario_index):
+
+        year_names = ["Critical", "Dry", "Below", "Above", "Wet"]
+        year_type = year_names[get_year_type-1]
+
+
+
+        net_demand_csv = pd.read_csv("s3_imports/NetDemand_belowTulloch.csv", index_col=[0])
+        net_demand_csv.index = pd.to_datetime(net_demand_csv.index)
+
+
     def _value(self, timestep, scenario_index):
-        multiplier = self.model.parameters['melones_storageValueConstant'].value(timestep, scenario_index)
-        storage_value_numerator = self.model.parameters['melones_storage_value_numerator'].value(timestep, scenario_index)
-        leading_multiplier = self.model.parameters['melones_storage_value_leading'].value(timestep, scenario_index)
-        # return leading_multiplier * math.exp(multiplier * (storage_value_numerator / self.model.parameters["node/New Melones Reservoir/Elevation"].value(timestep, scenario_index)))
-        return 1
+        out_flow = self.model.nodes["Tulloch Lake [node]"].volume[-1] + self.model.nodes["STN_below_Melons.2.2"].flow[-1] - self.model.parameters["node/Tulloch Lake/Storage Demand"].value(timestep, scenario_index)
+        net_demand = self.get_demand(timestep, scenario_index) + self.model.parameters["node/blwTullochPH/Requirement"].value(timestep, scenario_index)
+        return max(net_demand, min(out_flow, 226.535))
 
     def value(self, timestep, scenario_index):
         return self._value(timestep, scenario_index)
