b2f7a9536a0bd3614c821c2cddf7dc33332cf049
Dan <dantran2016@gmail.com>
2019-09-19 11:55:29 -0700

    Finished logic for Tulloch Lake

--- examples/Stan_Model/Stan_Model/policies/node_Release_From_Tulloch_Lake.py
+++ examples/Stan_Model/Stan_Model/policies/node_Release_From_Tulloch_Lake.py
@@ -1,7 +1,7 @@
 import math
 import pandas as pd
 import numpy as np
-from utilities import get_year_type
+from utilities.get_year_type import getSJVI_WYT
 from parameters import WaterLPParameter
 from datetime import datetime, timedelta
 
@@ -9,15 +9,15 @@ from datetime import datetime, timedelta
 class node_Release_From_Tulloch_Lake(WaterLPParameter):
 
     def get_demand(self, timestep, scenario_index):
-
+        million_m3day_to_m3sec = 11.5740740741
         year_names = ["Critical", "Dry", "Below", "Above", "Wet"]
-        year_type = year_names[get_year_type-1]
+        timestep_datetime = timestep.datetime
+        timestep_datetime = datetime(1990, timestep_datetime.month, timestep_datetime.day)
 
-
-
+        year_type = year_names[getSJVI_WYT(timestep)-1]
         net_demand_csv = pd.read_csv("s3_imports/NetDemand_belowTulloch.csv", index_col=[0])
-        net_demand_csv.index = pd.to_datetime(net_demand_csv.index)
 
+        return net_demand_csv.loc[str(timestep_datetime.date()), year_type] / million_m3day_to_m3sec
 
     def _value(self, timestep, scenario_index):
         out_flow = self.model.nodes["Tulloch Lake [node]"].volume[-1] + self.model.nodes["STN_below_Melons.2.2"].flow[-1] - self.model.parameters["node/Tulloch Lake/Storage Demand"].value(timestep, scenario_index)
