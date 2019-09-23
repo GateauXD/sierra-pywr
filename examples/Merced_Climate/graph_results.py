c3fab06dee757f896ca89ea0085d155378761369
Dan <dantran2016@gmail.com>
2019-09-23 15:07:53 -0700

    Reverted CC_Graph to work with new scheme

--- examples/Merced_Climate/graph_results.py
+++ examples/Merced_Climate/graph_results.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.dates as mdates
 import seaborn as sns
 
-mean = False
+mean = True
 date_of_interest = [2070,2100]
 item_of_interest = "Lake McClure Inflow Flood Days"
 climate_scenarios = ["CanESM2_rcp45", "CanESM2_rcp85", "CNRM-CM5_rcp45", "CNRM-CM5_rcp85", "HadGEM2-ES_rcp45",
@@ -13,7 +13,7 @@ climate_columns = []
 for column_name in climate_scenarios:
     climate_columns.append(column_name + "/" + item_of_interest)
 # cms_to_af = 810.714402
-cms_to_af = 0
+cms_to_af = 1
 
 historic_csv = pd.read_csv("C:\\Users\\GateauXD\\Documents\\GitHub\\waterlp-pywr2-dan\\examples\\Merced_Model\\Graph.csv", index_col=[0])
 climate_change_csv = pd.read_csv("Graph.csv", index_col=[0])
@@ -75,35 +75,39 @@ time = ["Near Future (2030-2060)", "Far Future (2070-2100)"]
 index = 1
 overall_climate = overall_climate[climate_columns]
 overall_climate["Historic"] = overall_historic[item_of_interest]
+overall_items = []
+for item in climate_scenarios:
+    overall_items.append(item + "/" + item_of_interest)
 
-ax = sns.barplot(overall_climate.index, overall_climate.values)
-climate_scenarios.append("Historic")
+ax = quarter_climate.reset_index().plot(x="index", y=overall_items, kind="bar")
+plt.xticks(np.arange(4), ("OND", "JFM", "AMJ", "JAS"), rotation=0, fontsize="54")
+plt.yticks(fontsize="54")
+plt.setp(ax.spines.values(), linewidth=3)
+plt.setp(ax.get_legend().get_texts(), fontsize='54')
+plt.title("Difference McSwain PH Hydropower Production " + time[index], fontsize='60')
+plt.legend(climate_scenarios, fontsize='54')
+plt.ylabel("Difference from Historic (MWD)", fontsize='54')
+plt.xlabel("Quarter", fontsize='54')
+plt.savefig("Figures/outflow_flood_averages_" + time[index].split()[0])
 
-plt.xticks(np.arange(len(climate_scenarios)), climate_scenarios ,fontsize=35, rotation=45)
+ax = quarter_delta_change.reset_index().plot(x="index", y=climate_scenarios, kind="bar")
+plt.xticks(np.arange(4), ("OND", "JFM", "AMJ", "JAS"),rotation=0, fontsize="54")
+plt.yticks(fontsize="54")
+plt.setp(ax.spines.values(), linewidth=3)
+plt.setp(ax.get_legend().get_texts(), fontsize='54')
+plt.title("Difference McSwain PH Hydropower Production " + time[index], fontsize='60')
+plt.legend(climate_scenarios, fontsize='54')
+plt.ylabel("Difference from Historic (MWD)", fontsize='54')
+plt.xlabel("Quarter", fontsize='54')
+plt.savefig("Figures/outflow_flood_difference_" + time[index].split()[0])
+
+ax = quarter_percent_change.reset_index().plot(x="index",y=climate_scenarios, kind="bar")
+plt.xticks(np.arange(4), ("OND", "JFM", "AMJ", "JAS"),rotation=0, fontsize="54")
 plt.setp(ax.spines.values(), linewidth=3)
 plt.yticks(fontsize="54")
-plt.title("Lake McClure Inflow No. of flood days annually " + time[index], fontsize=60)
-plt.ylabel("No. of days", fontsize='54')
-plt.savefig("Figures/30_lake_mcclure_inflow_averages_" + time[index].split()[0])
-
-# ax = yearly_delta_change.reset_index().plot(x="index", y=climate_scenarios, kind="bar")
-# plt.xticks(rotation=0, fontsize="54")
-# plt.yticks(fontsize="54")
-# plt.setp(ax.spines.values(), linewidth=3)
-# plt.setp(ax.get_legend().get_texts(), fontsize='54')
-# plt.title("Difference McSwain PH Hydropower Production " + time[index], fontsize='60')
-# plt.legend(climate_scenarios, fontsize='54')
-# plt.ylabel("Difference from Historic (MWD)", fontsize='54')
-# plt.xlabel("Quarter", fontsize='54')
-# plt.savefig("Figures/outflow_flood_difference_" + time[index].split()[0])
-#
-# ax = yearly_percent_change.reset_index().plot(x="index", y=climate_scenarios, kind="bar")
-# plt.xticks(rotation=0, fontsize="54")
-# plt.setp(ax.spines.values(), linewidth=3)
-# plt.yticks(fontsize="54")
-# plt.title("Percent Difference McSwain PH Hydropower Production " + time[index], fontsize='60')
-# plt.legend(climate_scenarios, fontsize='54')
-# plt.ylabel("Percent Difference (%)", fontsize='54')
-# plt.xlabel("Quarter", fontsize='54')
-# plt.savefig("Figures/outflow_flood_percent_" + time[index].split()[0])
+plt.title("Percent Difference McSwain PH Hydropower Production " + time[index], fontsize='60')
+plt.legend(climate_scenarios, fontsize='54')
+plt.ylabel("Percent Difference (%)", fontsize='54')
+plt.xlabel("Quarter", fontsize='54')
+plt.savefig("Figures/outflow_flood_percent_" + time[index].split()[0])
 
