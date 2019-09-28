8856eb153955dc68a9450db1efd6201b9f06420b
Dan <dantran2016@gmail.com>
2019-09-28 10:49:39 -0700

    Added box graph scripts  + Modified Graph_results to return abs diff

--- examples/Merced_Climate/graph_box.py
+++ examples/Merced_Climate/graph_box.py
@@ -2,30 +2,144 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import matplotlib.dates as mdates
 import seaborn as sns
-
-date_of_interest = [1980,2060]
-#item_of_interest = "Lake McClure Inflow/flow"
-
-def generate_csv():
-    results_csv = pd.read_csv("near_monthly_storage_all_85.csv", index_col=[4])
-#    results_csv = results_csv.loc["{}-10".format(date_of_interest[0]):"{}-9".format(date_of_interest[1])]
-    graph_melt = pd.melt(results_csv,
-                         id_vars=["Year", "Scen", "Month"],
-                         value_vars=["Inflow"],
-                         value_name="values"
-                         )
-    return graph_melt
+import numpy as np
 
 
-index = 0
-time = ["Near Future (2046-2055)", "Far Future (2070-2100)"]
-graph_data = generate_csv()
+def change_month(month):
+    if month == 10:
+        return 1
+    elif month == 11:
+        return 2
+    elif month == 12:
+        return 3
+    elif month == 1:
+        return 4
+    elif month == 2:
+        return 5
+    elif month == 3:
+        return 6
+    elif month == 4:
+        return 7
+    elif month == 5:
+        return 8
+    elif month == 6:
+        return 9
+    elif month == 7:
+        return 10
+    elif month == 8:
+        return 11
+    else:
+        return 12
+seasonal = True
+pd.options.mode.chained_assignment = None
+results_hist_csv = pd.read_csv(r"Graph_Historic.csv", index_col= None)
+results_climate_csv = pd.read_csv(r"Graph_Climate.csv", index_col= None)
+###########################
+## Storage
+results_hist_csv = results_hist_csv.iloc[:,[0,2,3]]
+results_climate_csv_45 = results_climate_csv.iloc[:,[0,2,3,21,39,57]]
+results_climate_csv_85 = results_climate_csv.iloc[:,[0,2,12,30,48,66]]
+file_name = "Storage"
+y_label = "Volume (TAF)"
+y_lim_low = 0
+y_lim_high = 1200
+##Inflow
+# results_hist_csv = results_hist_csv.iloc[:,[0,2,6]]
+# results_climate_csv_45 = results_climate_csv.iloc[:,[0,2,4,22,40,58]]
+# results_climate_csv_85 = results_climate_csv.iloc[:,[0,2,13,31,49,67]]
+# file_name = "Inflow"
+# y_label = "Discharge (TAF)"
+# y_lim_low = 0
+# y_lim_high = 1500
+##Outflow
+# results_hist_csv = results_hist_csv.iloc[:,[0,2,7]]
+# results_climate_csv_45 = results_climate_csv.iloc[:,[0,2,5,23,41,59]]
+# results_climate_csv_85 = results_climate_csv.iloc[:,[0,2,14,32,50,68]]
+# file_name = "Outflow"
+# y_label = "Discharge (TAF)"
+# y_lim_low = 0
+# y_lim_high = 300
+##HP Production
+# results_hist_csv = results_hist_csv.iloc[:,[0,2,13]]
+# results_climate_csv_45 = results_climate_csv.iloc[:,[0,2,11,29,47,65]]
+# results_climate_csv_85 = results_climate_csv.iloc[:,[0,2,20,38,56,74]]
+# file_name = "HP"
+# y_label = "HP Production (MWh)"
+# y_lim_low = 0
+# y_lim_high = 40000
+###########################
+
+
+results_hist_csv.columns = ['Date','WY','Historic']
+results_hist_csv['Month'] = pd.DatetimeIndex(results_hist_csv['Date']).month
+results_hist_csv['Day'] = pd.DatetimeIndex(results_hist_csv['Date']).dayofyear
+
+results_hist_csv["WYT_Month"] = results_hist_csv["Month"].apply(lambda x: change_month(x))
+
+results_hist_csv_melt = pd.melt(results_hist_csv,
+                          id_vars=["Date", "WY", "Month", "WYT_Month", "Day"],
+                          value_vars=["Historic"],
+                          var_name=["Scen"],
+                          value_name="Values"
+                          )
+
+results_climate_csv_45.columns = ['Date','WY','CanESM2','CNRM-CM5','HadGEM2','MIROC5']
+results_climate_csv_85.columns = ['Date','WY','CanESM2','CNRM-CM5','HadGEM2','MIROC5']
+results_climate_csv_45['Month'] = pd.DatetimeIndex(results_climate_csv_45['Date']).month
+results_climate_csv_45['Day'] = pd.DatetimeIndex(results_climate_csv_45['Date']).dayofyear
+results_climate_csv_85['Month'] = pd.DatetimeIndex(results_climate_csv_85['Date']).month
+results_climate_csv_85['Day'] = pd.DatetimeIndex(results_climate_csv_85['Date']).dayofyear
+
+results_climate_csv_45["WYT_Month"] = results_climate_csv_45["Month"].apply(lambda x: change_month(x))
+results_climate_csv_85["WYT_Month"] = results_climate_csv_85["Month"].apply(lambda x: change_month(x))
+
+
+
+results_climate_csv_melt_45 = pd.melt(results_climate_csv_45,
+                          id_vars=["Date", "WY", "Month", "WYT_Month","Day"],
+                          value_vars=['CanESM2','CNRM-CM5','HadGEM2','MIROC5'],
+                          var_name=["Scen"],
+                          value_name="Values"
+                          )
+results_climate_csv_melt_85 = pd.melt(results_climate_csv_85,
+                          id_vars=["Date", "WY", "Month", "WYT_Month", "Day"],
+                          value_vars=['CanESM2','CNRM-CM5','HadGEM2','MIROC5'],
+                          var_name=["Scen"],
+                          value_name="Values"
+                          )
+graph_data_45 = pd.concat([results_hist_csv_melt,results_climate_csv_melt_45])
+graph_data_85 = pd.concat([results_hist_csv_melt,results_climate_csv_melt_85])
+
+if seasonal:
+    # Seasonal
+    graph_data_45 = graph_data_45.groupby(graph_data_45["WYT_Month"]).mean()
+    graph_data_45 = graph_data_45.groupby(np.arange(len(graph_data_45)) // 3).mean()
+    graph_data_85 = graph_data_85.groupby(graph_data_85["WYT_Month"]).mean()
+    graph_data_85 = graph_data_85.groupby(np.arange(len(graph_data_85)) // 3).mean()
+
+
+plt_color = ["#505050", "#c1f215", "#00cdcd", "#ffa500", "#c6b6e0"]
+sns.set(style="whitegrid", rc={'figure.figsize': (20.7, 13.27)}, font_scale=2.0)
+plt.rcParams["figure.figsize"] = [16,9]
+pp = sns.boxplot(x="index", y="Values", hue="Scen", data=graph_data_45,palette=plt_color)
+# sns.lineplot(x="Day", y="Values", hue="Scen", data=graph_data_45, palette=plt_color)
+plt.xticks(np.arange(4), ["OND", "JFM", "AMJ", "JAS"])
+plt.xlabel("Quarter")
+plt.ylabel(y_label)
+plt.ylim(y_lim_low, y_lim_high)
+plt.savefig("Figures/"+ file_name + "_BoxQuarter_45.png")
+# plt.savefig("Figures/" + file_name + "_LineMonthly_45.png")
+plt.clf()
 
 plt.rcParams["figure.figsize"] = [16,9]
-sns.set(style="whitegrid", rc={'figure.figsize': (20.7, 13.27)}, font_scale=1.5)
-sns.boxplot(x="Month", y="values", hue="Scen", data=graph_data)
-plt.title("Lake McClure Storage - All Scenarios RCP 8.5 - {}".format(time[index]))
-plt.xlabel("Month")
-#plt.ylabel(r"Flow Value (million $m^{3}$)")
-plt.ylabel(r"Storage (million cubic meter)")
-plt.savefig("Figures/{}_monthly_mcclureStorage_all_85.png".format(time[index].split()[0].lower()))
+sns.boxplot(data=graph_data_85.reset_index(), x="index", y="Values", hue="Scen",palette=plt_color)
+# sns.lineplot(x="Day", y="Values", hue="Scen", data=graph_data_85, palette=plt_color)
+plt.xticks(np.arange(4), ["OND", "JFM", "AMJ", "JAS"])
+plt.xlabel("Quarter")
+plt.ylabel(y_label)
+plt.ylim(y_lim_low, y_lim_high)
+plt.savefig("Figures/"+ file_name + "_BoxQuarter_85.png")
+# plt.savefig("Figures/"+ file_name + "_LineMonthly_85.png")
+
+#sns.lineplot(x="Day", y="Values", hue="Scen",estimator=None, lw=1, data=graph_data, legend=False)
+
--- examples/Merced_Climate/graph_results.py
+++ examples/Merced_Climate/graph_results.py
@@ -5,63 +5,73 @@ import matplotlib.dates as mdates
 import seaborn as sns
 
 mean = True
-date_of_interest = [2046,2055]
-item_of_interest = "node/Basin Outflow/flow"
+date_of_interest = [2010,2031]
+item_of_interests = ["Lake McClure Storage", "node/Basin Outflow/flow", "Lake McClure Inflow", "Hydropower Production"]
 climate_scenarios = ["CanESM2_rcp45", "CanESM2_rcp85", "CNRM-CM5_rcp45", "CNRM-CM5_rcp85", "HadGEM2-ES_rcp45",
                      "HadGEM2-ES_rcp85", "MIROC5_rcp45", "MIROC5_rcp85"]
-climate_columns = []
-for column_name in climate_scenarios:
-    climate_columns.append(column_name + "/" + item_of_interest)
+# climate_columns = []
+# for column_name in climate_scenarios:
+#     climate_columns.append(column_name + "/" + item_of_interest)
 # cms_to_af = 810.714402
 cms_to_af = 1
 
-historic_csv = pd.read_csv("C:\\Users\\GateauXD\\Documents\\GitHub\\waterlp-pywr2-dan\\examples\\Merced_Model\\Graph.csv", index_col=[0])
-climate_change_csv = pd.read_csv("Graph.csv", index_col=[0])
+historic_csv = pd.read_csv("Graph_Historic.csv", index_col=[0])
+climate_change_csv = pd.read_csv("Graph_Climate.csv", index_col=[0])
 historic_csv.index = pd.to_datetime(historic_csv.index)
 climate_change_csv.index = pd.to_datetime(climate_change_csv.index)
 # Get date of interest
-climate_change_csv = climate_change_csv.loc["{}-01-01".format(date_of_interest[0]):"{}-12-31".format(date_of_interest[1])]
+climate_change_csv = climate_change_csv.loc["{}-10-01".format(date_of_interest[0]):"{}-9-30".format(date_of_interest[1])]
 
+quarter_delta_change = pd.DataFrame()
+quarter_percent_change = pd.DataFrame()
+yearly_delta_change = pd.DataFrame()
+yearly_percent_change = pd.DataFrame()
+overall_delta_climate = pd.Series()
+overall_percent_climate = pd.Series()
 if mean:
-    # Overall Mean
-    overall_historic = historic_csv.mean()
-    overall_climate = climate_change_csv.mean()
-    # Annual Means
-    yearly_historic = historic_csv.groupby(historic_csv.index.year).mean()
-    yearly_climate = climate_change_csv.groupby(climate_change_csv.index.year).mean()
-    # Convert to Monthly Means over all Years
-    monthly_historic = historic_csv.groupby(historic_csv.index.month).mean()
-    monthly_climate = climate_change_csv.groupby(climate_change_csv.index.month).mean()
-    # Convert Monthly to Quarterly mean
-    quarter_historic = monthly_historic.groupby(np.arange(len(monthly_historic)) // 3).mean()
-    quarter_climate = monthly_climate.groupby(np.arange(len(monthly_climate)) // 3).mean()
-    # Swap quarters around so OND is first
-    quarter_historic = quarter_historic.loc[[3, 0, 1, 2]]
-    quarter_climate = quarter_climate.loc[[3, 0, 1, 2]]
+    for item_of_interest in item_of_interests:
+        # Overall Mean
+        overall_historic = historic_csv.mean()
+        overall_climate = climate_change_csv.mean()
+        # Annual Means
+        yearly_historic = historic_csv.groupby(historic_csv["Water_Year"]).mean()
+        yearly_climate = climate_change_csv.groupby(climate_change_csv["Water_Year"]).mean()
+        # Convert to Monthly Means over all Years
+        monthly_historic = historic_csv.groupby(historic_csv.index.month).mean()
+        monthly_climate = climate_change_csv.groupby(climate_change_csv.index.month).mean()
+        # Convert Monthly to Quarterly mean
+        quarter_historic = monthly_historic.groupby(np.arange(len(monthly_historic)) // 3).mean()
+        quarter_climate = monthly_climate.groupby(np.arange(len(monthly_climate)) // 3).mean()
+        # Swap quarters around so OND is first
+        quarter_historic = quarter_historic.loc[[3, 0, 1, 2]]
+        quarter_climate = quarter_climate.loc[[3, 0, 1, 2]]
 
-    # Change units of flow from mcm to af
-    quarter_historic[item_of_interest] = quarter_historic[item_of_interest] * cms_to_af
-    for climate_scenario in climate_scenarios:
-        quarter_climate[climate_scenario + "/" + item_of_interest] = quarter_climate[
-                                                                         climate_scenario + "/" + item_of_interest] * cms_to_af
+        # Change units of flow from mcm to af
+        quarter_historic[item_of_interest] = quarter_historic[item_of_interest] * cms_to_af
+        for climate_scenario in climate_scenarios:
+            quarter_climate[climate_scenario + "/" + item_of_interest] = quarter_climate[
+                                                                             climate_scenario + "/" + item_of_interest] * cms_to_af
 
-    # Get the Difference and Percent Difference from historic
-    quarter_delta_change = pd.DataFrame()
-    quarter_percent_change = pd.DataFrame()
-    yearly_delta_change = pd.DataFrame()
-    yearly_percent_change = pd.DataFrame()
-    for climate_scenario in climate_scenarios:
-        quarter_delta_change[climate_scenario] = (
+        # Get the Difference and Percent Difference from historic
+        for climate_scenario in climate_scenarios:
+            quarter_delta_change[climate_scenario + "/" + item_of_interest] = abs(
                     quarter_climate[climate_scenario + "/" + item_of_interest] - quarter_historic[item_of_interest])
-        quarter_percent_change[climate_scenario] = 100 * (
+            quarter_percent_change[climate_scenario + "/" + item_of_interest] = 100 * (
                     quarter_climate[climate_scenario + "/" + item_of_interest] - quarter_historic[item_of_interest]) / \
-                                                   quarter_historic[item_of_interest]
-        yearly_delta_change[climate_scenario] = (
-                    yearly_climate[climate_scenario + "/" + item_of_interest] - yearly_historic[item_of_interest])
-        yearly_percent_change[climate_scenario] = 100 * (
-                    yearly_climate[climate_scenario + "/" + item_of_interest] - yearly_historic[item_of_interest]) / \
-                                                  yearly_historic[item_of_interest]
-# Flood Days
+                                                       quarter_historic[item_of_interest]
+            yearly_delta_change[climate_scenario + "/" + item_of_interest] = abs(
+                    yearly_climate[climate_scenario + "/" + item_of_interest] - yearly_historic[item_of_interest].values)
+            yearly_percent_change[climate_scenario + "/" + item_of_interest] = 100 * (
+                    yearly_climate[climate_scenario + "/" + item_of_interest] - yearly_historic[item_of_interest].values) / \
+                                                      yearly_historic[item_of_interest].values
+
+            overall_delta_climate[climate_scenario + "/" + item_of_interest] = abs(
+                    overall_climate[climate_scenario + "/" + item_of_interest] - overall_historic[item_of_interest])
+
+            overall_percent_climate[climate_scenario + "/" + item_of_interest] = 100 * (
+                    overall_climate[climate_scenario + "/" + item_of_interest] - overall_historic[item_of_interest]) / \
+                                                      overall_historic[item_of_interest]
+    # Flood Days
 else:
     # Annual Sums
     yearly_historic = historic_csv.groupby(historic_csv.index.year).sum()
@@ -70,8 +80,8 @@ else:
     monthly_historic = historic_csv.groupby(historic_csv.index.month).sum()
     monthly_climate = climate_change_csv.groupby(climate_change_csv.index.month).sum()
     # Convert Monthly to Quarterly mean
-    quarter_historic = monthly_historic.groupby(np.arange(len(monthly_historic)) // 3).sum()/30
-    quarter_climate = monthly_climate.groupby(np.arange(len(monthly_climate)) // 3).sum()/10
+    quarter_historic = monthly_historic.groupby(np.arange(len(monthly_historic)) // 3).mean()
+    quarter_climate = monthly_climate.groupby(np.arange(len(monthly_climate)) // 3).mean()
     # Swap quarters around so OND is first
     quarter_historic = quarter_historic.loc[[3, 0, 1, 2]]
     quarter_climate = quarter_climate.loc[[3, 0, 1, 2]]
@@ -79,25 +89,28 @@ else:
     overall_historic = yearly_historic.mean()
     overall_climate = yearly_climate.mean()
 
-plt.rcParams["figure.figsize"] = [50,35]
-time = ["Near Future (2046-2055)", "Far Future (2070-2100)"]
-index = 0
-quarter_climate["Historic"] = quarter_historic[item_of_interest]
-overall_items = ["Historic"]
-for item in climate_scenarios:
-    overall_items.append(item + "/" + item_of_interest)
-climate_scenarios.insert(0, "Historic")
+overall_delta_climate.to_csv("merced/s3_imports/overall_delta_climate.csv")
+overall_percent_climate.to_csv("merced/s3_imports/overall_percent_climate.csv")
 
-ax = quarter_climate.reset_index().plot(x="index", y=overall_items, kind="bar")
-plt.xticks(np.arange(4), ("OND", "JFM", "AMJ", "JAS"), rotation=0, fontsize="54")
-plt.yticks(fontsize="54")
-plt.setp(ax.spines.values(), linewidth=3)
-plt.setp(ax.get_legend().get_texts(), fontsize='54')
-plt.title("Outflow from the Merced Basin " + time[index], fontsize='60')
-plt.legend(climate_scenarios, fontsize='54')
-plt.ylabel("Average Outflow (TAF)", fontsize='54')
-plt.xlabel("Quarter", fontsize='54')
-plt.savefig("Figures/outflow_merced_avg_" + time[index].split()[0])
+# plt.rcParams["figure.figsize"] = [50,35]
+# time = ["Near Future (2046-2055)", "Far Future (2070-2100)"]
+# index = 0
+# quarter_climate["Historic"] = quarter_historic[item_of_interest]
+# overall_items = ["Historic"]
+# for item in climate_scenarios:
+#     overall_items.append(item + "/" + item_of_interest)
+# climate_scenarios.insert(0, "Historic")
+#
+# ax = quarter_climate.reset_index().plot(x="index", y=overall_items, kind="bar")
+# plt.xticks(np.arange(4), ("OND", "JFM", "AMJ", "JAS"), rotation=0, fontsize="54")
+# plt.yticks(fontsize="54")
+# plt.setp(ax.spines.values(), linewidth=3)
+# plt.setp(ax.get_legend().get_texts(), fontsize='54')
+# plt.title("Outflow from the Merced Basin " + time[index], fontsize='60')
+# plt.legend(climate_scenarios, fontsize='54')
+# plt.ylabel("Average Outflow (TAF)", fontsize='54')
+# plt.xlabel("Quarter", fontsize='54')
+# plt.savefig("Figures/outflow_merced_avg_" + time[index].split()[0])
 
 # ax = quarter_delta_change.reset_index().plot(x="index", y=climate_scenarios, kind="bar")
 # plt.xticks(np.arange(4), ("OND", "JFM", "AMJ", "JAS"),rotation=0, fontsize="54")
@@ -119,9 +132,4 @@ plt.savefig("Figures/outflow_merced_avg_" + time[index].split()[0])
 # plt.ylabel("Percent Difference (%)", fontsize='54')
 # plt.xlabel("Quarter", fontsize='54')
 # plt.savefig("Figures/outflow_flood_percent_" + time[index].split()[0])
-
-quarter_climate.to_csv("merced/s3_imports/quarter_climate_avg.csv")
-yearly_climate.to_csv("merced/s3_imports/yearly_climate_avg.csv")
-quarter_historic.to_csv("merced/s3_imports/quarter_hist_avg.csv")
-yearly_historic.to_csv("merced/s3_imports/yearly_hist_avg.csv")
 
