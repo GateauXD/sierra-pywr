ace3b8f3e65f17c9f13b648c59b1d33da0c10207
Dan <dantran2016@gmail.com>
2019-10-03 13:08:23 -0700

    Added logic for New Melones storage value + elevation curve

--- examples/Stan_Model/Stan_Model/_parameters/node_New_Melones_Reservoir_Storage_Value.py
+++ examples/Stan_Model/Stan_Model/_parameters/node_New_Melones_Reservoir_Storage_Value.py
@@ -11,8 +11,8 @@ class node_New_Melones_Reservoir_Storage_Value(WaterLPParameter):
         multiplier = self.model.parameters['melones_storageValueConstant'].value(timestep, scenario_index)
         storage_value_numerator = self.model.parameters['melones_storage_value_numerator'].value(timestep, scenario_index)
         leading_multiplier = self.model.parameters['melones_storage_value_leading'].value(timestep, scenario_index)
-        # return leading_multiplier * math.exp(multiplier * (storage_value_numerator / self.model.parameters["node/New Melones Reservoir/Elevation"].value(timestep, scenario_index)))
-        return 1
+        return leading_multiplier * math.exp(multiplier * (storage_value_numerator / self.model.parameters["node/New Melones Lake/Elevation"].value(timestep, scenario_index)))
+
 
     def value(self, timestep, scenario_index):
         return self._value(timestep, scenario_index)
