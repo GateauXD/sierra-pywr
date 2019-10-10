bca52693041ccdf76c569055dbf9902a1c5b4cdc
Dan <dantran2016@gmail.com>
2019-10-10 10:00:27 -0700

    Fixed logic for New Melones storage value + Added script to graph Calibration results

--- examples/Stan_Model/Stan_Model/_parameters/node_New_Melones_Reservoir_Storage_Value.py
+++ examples/Stan_Model/Stan_Model/_parameters/node_New_Melones_Reservoir_Storage_Value.py
@@ -8,10 +8,11 @@ from datetime import datetime, timedelta
 class node_New_Melones_Reservoir_Storage_Value(WaterLPParameter):
 
     def _value(self, timestep, scenario_index):
+        min_elevation = 182.87
+        max_elevation = 341.05
         multiplier = self.model.parameters['melones_storageValueConstant'].value(timestep, scenario_index)
         leading_multiplier = self.model.parameters['melones_storage_value_leading'].value(timestep, scenario_index)
-        return leading_multiplier * math.exp((multiplier * (self.model.nodes["New Melones Lake [node]"].max_volume - self.model.nodes["New Melones Lake [node]"].min_volume)) / self.model.parameters["node/New Melones Lake/Elevation"].value(timestep, scenario_index))
-
+        return -1 * leading_multiplier * math.exp((multiplier * (max_elevation - min_elevation) / self.model.parameters["node/New Melones Lake/Elevation"].value(timestep, scenario_index)))
 
     def value(self, timestep, scenario_index):
         return self._value(timestep, scenario_index)
